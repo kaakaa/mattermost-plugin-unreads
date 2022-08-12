c45161a800859bb899a1e5304453cb1bfe3917a9
kaakaa <stooner.hoe@gmail.com>
2022-08-12 14:16:51 +0900

    lint: fix lint errors

--- webapp/src/types/mattermost-webapp/index.d.ts
+++ webapp/src/types/mattermost-webapp/index.d.ts
@@ -4,5 +4,6 @@ export interface PluginRegistry {
     registerRightHandSidebarComponent(component: React.ElementType, title: string);
     registerChannelHeaderButtonAction(icon: React.ElementType, action, dropdownText: string, tooltipText: string)
     registerAppBarComponent(iconUrl: string, action: () => any, tooltip_text: string | React.ElementType);
+
     // Add more if needed from https://developers.mattermost.com/extend/plugins/webapp/reference
 }
